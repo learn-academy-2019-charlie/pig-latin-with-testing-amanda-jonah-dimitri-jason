5f9ec555378b3cd39037f9f6ce0e11ea47b6bd85
learn <learn@learn08.local>
2019-06-11 16:30:18 -0700

    day two modifications: complete project

--- piglatin.js
+++ piglatin.js
@@ -1,1 +1,85 @@
 
+//declare string variable
+const testStr = "Testing away string quail squeal"
+// const is being called in the primary function to split out the string into an array of words.
+const plSplit = (str) => {
+    return newArr = str.split(" ")
+}
+// if word starts with vowel run this function
+var plStartV = (word) => {
+    return word.concat("way")
+}
+//if the word starts with a consonant then run this function!
+var plCons = (word) => {
+    let posOfFirstV
+    for (let i = 0; i < word.length; i++){
+        if(vowels.includes(word.charAt(i))) {
+            posOfFirstV = i
+            break
+        }
+    }
+    let tail = word.slice(0, posOfFirstV)
+    let head = word.slice(posOfFirstV, word.length)
+    return `${head}${tail}ay`
+}
+//If the word starts with a q run this funciton!
+var plQ = (word) => {
+    let posOfFirstQ
+
+    if(q.includes(word.charAt(0))) {
+        posOfFirstQ = 0
+    } else if(q.includes(word.charAt(1))){
+         posOfFirstQ = 1
+     }
+    let tailQ = word.slice(0, posOfFirstQ + 2)
+    let headQ = word.slice(posOfFirstQ + 2, word.length)
+    return `${headQ}${tailQ}ay`
+}
+//if the word starts with a y run this function!
+var plY = (word) => {
+    let newWord = word.substr(1,word.length)+word.substr(0,1) +"ay"
+    return newWord
+}
+
+const vowels = ["a","e","i","o","u","y"]
+const q = ["q"]
+const plMain = ()=>{
+    let words = document.getElementById("user-input").value
+    if(words!==""){
+    let splitWords = plSplit(words)
+    // console.log(splitWords);
+    let newWords=[]
+    for(let i=0;i<splitWords.length;i++){
+        let tempString = splitWords[i]
+        // console.log(tempString);
+        if("y".includes(tempString.charAt(0))) {
+//run *robust const function for y*  to remove all letters up to first vowel and move the end
+        newWords.push(plY(tempString))
+        }
+        else if(vowels.includes(tempString.charAt(0))){
+            newWords.push(plStartV(tempString))
+        }
+// if word starts with qu
+        else if(q.includes(tempString.charAt(0)) || q.includes(tempString.charAt(1))) {
+            newWords.push(plQ(tempString))
+            console.log(newWords);
+//run through *qu function** to move both q and u to the end
+
+        }  else if(vowels.includes(tempString.charAt(0)) === false){
+//run through *qu function** to move both q and u to the end
+            newWords.push(plCons(tempString))
+// }else if //the first letter is y
+        }else{
+            newWords.push("null")
+        }
+    //
+    // else // catch-all if not a string of characters, or whatever
+    console.log(newWords);
+    document.getElementById("output").innerHTML = newWords.join(" ")
+}
+}else{
+    document.getElementById("output").innerHTML =" Please enter words"
+}
+
+}
+// console.log(plMain(testStr))
--- piglatin.test.js
+++ piglatin.test.js
@@ -3,68 +3,100 @@
 //deal 'else' ask for new input if not a string
 //if the word is one vowel, add '-way' to the end.
 
-//declare string variable
-const testStr = "Testing a string"
-//result of our function -> "testing a-way string
-// const plSplit = (str) => {
-//     return newArr = str.split(" ")
-//
-// }
-// console.log(plSplit(testStr))
+//**TEST SUITE**//
 
-// describe ("plSplit", () => {
-//     test ("change string to an array where each word is an index", () => {
-//         expect(plSplit(testStr)).toEqual([ 'Testing','a','string' ])
-//     })
-// })
-
-// var plStartV = (word) => {
-//     let vowels = ["a","e","i","o","u"]
-//     let splitWord = word.split("")
-//     if(vowels.indexOf(splitWord[0])!== -1){
-//         return word.concat("-way")
-//     }
-// }
-// console.log(plStartV("a"));
-
-// var plCons = (word) => {
-//     let vowels = ["a","e","i","o","u"]
-//     let splitWord = word.split("")
-//     let newArr = []
-//     for (let i = 0; i < splitWord.length; i++){
-//         if(vowels.indexOf(splitWord[i])=== -1){
-//             newArr.push(splitWord[i])
-//             splitWord.shift()
-//             i = i - 1
-//         } else {
-//             newArr = newArr.join("")
-//             splitWord = splitWord.join("")
-//             return `${splitWord}-${newArr}ay`
-//         }
-//     }
-// }
-
-var plCons = (word) => {
-    let vowels = ["a","e","i","o","u"]
-    let splitWord = word.split("")
-    let posOfFirstV = splitWord.map((value,index) => {
-            if(vowels.indexOf(value) !== - 1 ){
-                return index
-            }else{
-                return "No vowels present"
-            }
+describe ("plSplit", () => {
+    test ("change string to an array where each word is an index", () => {
+        expect(plSplit("Testing away string quail squeal")).toEqual(expect.arrayContaining(["Testing","away","string"]))
     })
+})
+describe ("plMain", () => {
+    test ("Runs plMain and test the function within", () => {
+        expect(plMain("Testing away string quail squeal yellow yay rhythm")).toEqual("estingTay awayway ingstray ailquay ealsquay ellowyay ayyay ythmrhay")
+    })
+})
+
+
+
+
+
+//declare string variable
+const testStr = "Testing away string quail squeal"
+// const is being called in the primary function to split out the string into an array of words.
+const plSplit = (str) => {
+    return newArr = str.split(" ")
+}
+// if word starts with vowel run this function
+var plStartV = (word) => {
+    return word.concat("way")
+}
+//if the word starts with a consonant then run this function!
+var plCons = (word) => {
+    let posOfFirstV
+    for (let i = 0; i < word.length; i++){
+        if(vowels.includes(word.charAt(i))) {
+            posOfFirstV = i
+            break
+        }
+    }
+    let tail = word.slice(0, posOfFirstV)
+    let head = word.slice(posOfFirstV, word.length)
+    return `${head}${tail}ay`
+}
+//If the word starts with a q run this funciton!
+var plQ = (word) => {
+    let posOfFirstQ
+
+    if(q.includes(word.charAt(0))) {
+        posOfFirstQ = 0
+    } else if(q.includes(word.charAt(1))){
+         posOfFirstQ = 1
+     }
+    let tailQ = word.slice(0, posOfFirstQ + 2)
+    let headQ = word.slice(posOfFirstQ + 2, word.length)
+    return `${headQ}${tailQ}ay`
+}
+//if the word starts with a y run this function!
+var plY = (word) => {
+    let newWord = word.substr(1,word.length)+word.substr(0,1) +"ay"
+    return newWord
 }
 
-console.log(plCons("spleen"));
+const vowels = ["a","e","i","o","u","y"]
+const q = ["q"]
+const plMain = ()=>{
+    let words = document.getElementById("user-input").value
+    let splitWords = plSplit(words)
+    // console.log(splitWords);
+    let newWords=[]
+    for(let i=0;i<splitWords.length;i++){
+        let tempString = splitWords[i]
+        // console.log(tempString);
+        if("y".includes(tempString.charAt(0))) {
+//run *robust const function for y*  to remove all letters up to first vowel and move the end
+        newWords.push(plY(tempString))
+        }
+        else if(vowels.includes(tempString.charAt(0))){
+            newWords.push(plStartV(tempString))
+        }
+// if word starts with qu
+        else if(q.includes(tempString.charAt(0)) || q.includes(tempString.charAt(1))) {
+            newWords.push(plQ(tempString))
+            console.log(newWords);
+//run through *qu function** to move both q and u to the end
 
-// const plMain = (words)=>{
-//     let splitWords = plSplit(words)
-//     console.log(splitWords);
-//     let newWords=[]
-//     for(let i=0;i<splitWords.length;i++){
-//         newWords.push(plStartV(splitWords[i]))
-//     }
-//     return newWords
-// }
-// console.log(plMain(testStr));
+        }  else if(vowels.includes(tempString.charAt(0)) === false){
+//run through *qu function** to move both q and u to the end
+            newWords.push(plCons(tempString))
+// }else if //the first letter is y
+        }else{
+            newWords.push("null")
+        }
+    //
+    // else // catch-all if not a string of characters, or whatever
+
+    }
+    console.log(newWords);
+    document.getElementById("output").innerHTML = newWords.join(" ")
+}
+// console.log(plMain(testStr))
